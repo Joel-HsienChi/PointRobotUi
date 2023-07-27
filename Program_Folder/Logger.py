3b5a1615c160b539420b6a8fcedf5a5d9afaa016
Joel-HsienChi <71637398+Joel-HsienChi@users.noreply.github.com>
2023-07-27 15:49:59 +0800

    Update Logger.py

--- Program_Folder/Logger.py
+++ Program_Folder/Logger.py
@@ -1,3 +1,19 @@
+'''
+This class is a simple logging class that provides easier logging experience. 
+To use this loggin class, just create a logger object with desired parameter. 
+For instance, to create a logger named: TestLogger with a level of INFO and located at "Log_Folder/Test.log",
+
+You will have to declare a variable like following:
+
+    TestLogger = logger_class("test_logger", logging.INFO, "Log_Folder/Test.log")
+
+Then, to log the message into log file, call the created logger with .logger, then you will be able to log the 
+message with desired level.
+
+For instance to log a message into log file at info level, you will have to do it like:
+
+    TestLogger.logger.info("This is a test info message )
+'''
 import logging
 
 
